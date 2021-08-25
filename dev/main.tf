01274667189f81f15097546f6e827795c4a90484
Jonny Graves <jalgraves@gmail.com>
2021-08-25 13:51:14 -0400

    Even more Atlantis testing (#7)

--- dev/main.tf
+++ dev/main.tf
@@ -26,3 +26,12 @@ module "web_sg" {
   description = "Security group for web ingress"
   vpc_id = module.vpc.vpc_id
 }
+
+module "alb" {
+  source = "../modules/alb/"
+  name = "main-alb"
+  vpc_id = module.vpc.vpc_id
+  subnets = module.vpc.private_subnets
+  security_groups = [module.web_sg.security_group_id]
+  description = "ALB for Beantown Pub"
+}
--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -31,3 +31,27 @@ variable "target_groups" {
   type = any
   default = []
 }
+
+variable "enable_http2" {
+  description = "Indicates whether HTTP/2 is enabled in ALB"
+  type = bool
+  default = false
+}
+
+variable "create_lb" {
+  description = "Controls if ALB should be created"
+  type = bool
+  default = false
+}
+
+variable "internal" {
+  description = "Determines if ALB is internal or externally facing"
+  type = bool
+  default = false
+}
+
+variable "http_tcp_listeners" {
+  description = "A list of maps describing the HTTP listeners or TCP ports for this ALB. Required key/values: port, protocol. Optional key/values: target_group_index (defaults to http_tcp_listeners[count.index])"
+  type = any
+  default = []
+}
