3d908a018947a1f68b966ae5ab6373c1f2a51c43
jonny graves <jonny.graves@hqo.co>
2022-02-23 18:05:48 -0500

    Cleanup

--- tfc/main.tf
+++ tfc/main.tf
@@ -18,57 +18,6 @@ resource "tfe_team" "owners" {
   organization = tfe_organization.beantown.name
 }
 
-resource "tfe_workspace" "dev" {
-  name              = "dev"
-  description       = "Workspace for dev infrastructure"
-  organization      = tfe_organization.beantown.name
-  execution_mode    = "remote"
-  tag_names         = ["dev"]
-  terraform_version = "1.1.3"
-  working_directory = "dev"
-  vcs_repo {
-    branch             = "master"
-    identifier         = "beantownpub/infrastructure"
-    ingress_submodules = false
-    oauth_token_id     = var.github_oauth_token
-  }
-}
-
-resource "tfe_workspace" "pilot" {
-  name              = "pilot"
-  description       = "Workspace for pilot infrastructure"
-  organization      = tfe_organization.beantown.name
-  execution_mode    = "remote"
-  tag_names         = ["pilot"]
-  terraform_version = "1.1.3"
-  working_directory = "pilot"
-  vcs_repo {
-    branch             = "master"
-    identifier         = "beantownpub/infrastructure"
-    ingress_submodules = false
-    oauth_token_id     = var.github_oauth_token
-  }
-}
-
-resource "tfe_workspace" "prod" {
-  name                  = "prod"
-  description           = "Workspace for prod infrastructure"
-  organization          = tfe_organization.beantown.name
-  execution_mode        = "remote"
-  file_triggers_enabled = true
-  queue_all_runs        = true
-  speculative_enabled   = true
-  tag_names             = ["prod"]
-  terraform_version     = "1.1.3"
-  working_directory     = "prod"
-  vcs_repo {
-    branch             = "master"
-    identifier         = "beantownpub/infrastructure"
-    ingress_submodules = false
-    oauth_token_id     = var.github_oauth_token
-  }
-}
-
 resource "tfe_workspace" "dns" {
   name                  = "dns"
   description           = "Workspace for managing Route53 resources"
